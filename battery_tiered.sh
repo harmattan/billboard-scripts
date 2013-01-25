46a8c06435f11cb21fca0ef4c84b2bc8427f929c
thedead1440 <ars.2610@gmail.com>
2013-01-25 17:18:42 +0800

    Correct Bar
    Script corrected to reflect possible fallacies in the coloured bar representation. Correction provided by Thomas Perl; thp.

--- battery_tiered.sh
+++ battery_tiered.sh
@@ -2,17 +2,18 @@
 
 # Display Battery Percentage with a Colored Bar in tiered range #
 # Author: thedead1440
+# Bar range corrected by Thomas Perl aka thp
 
 status=$(qdbus --system org.freedesktop.Hal /org/freedesktop/Hal/devices/bme org.freedesktop.Hal.Device.GetProperty battery.charge_level.percentage)
 if [[ $status -ge 50 ]]
 then
-echo -n "{{green}} Battery $status % " "{{=0.$status}}"
+echo -n "{{green}} Battery $status % " "{{=$(($status/100)).$((($status%100)/10))$(($status%10))}}"
 else if [[ $status -ge 25 && $staus -le 49 ]]
 then
-echo -n "{{yellow}} Battery $status % " "{{=0.$status}}"
+echo -n "{{yellow}} Battery $status % " "{{=$(($status/100)).$((($status%100)/10))$(($status%10))}}"
 else if [[ $status -le 24 ]]
 then 
-echo -n "{{red}} Battery $status % " "{{=0.$status}}"
+echo -n "{{red}} Battery $status % " "{{=$(($status/100)).$((($status%100)/10))$(($status%10))}}"
 fi
 fi
 fi
