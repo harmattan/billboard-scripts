294d126777e5b7a55308dcdeaeafd145b46bdb37
Thomas Perl <m@thp.io>
2013-02-06 16:36:56 +0100

    Rewrite "days_remaining_in_year" in Python

--- days_remaining_in_year.py
+++ days_remaining_in_year.py
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/usr/bin/python
 # Days Remaining in the Current Year
 # Author: Thomas Perl
 
+import datetime
+
+today = datetime.datetime.now()
+day_of_year = int(today.strftime('%j'))
+
 # Leap year check will work until 2100 (mod 100 and mod 400 rules ignored)
 # See http://en.wikipedia.org/wiki/Leap_year#Algorithm
-leap_year=$(($(date +%Y) % 4 == 0))
+leap_year = (today.year % 4 == 0)
 
-echo $((365 + $leap_year - $(date +%j)))
+print (366 if leap_year else 365) - day_of_year
 
